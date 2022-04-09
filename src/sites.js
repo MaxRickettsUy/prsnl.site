03bc6c8e7b072b5731c841d1e763abead47e03c0
Max Ricketts-Uy <mrickettsuy@protonmail.com>
2022-04-08 22:03:10 -0400

    Add templating with js

---
+++ src/sites.js
@@ -0,0 +1,16 @@
+const sites = [
+    {"url": "https://brianlovin.com", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649297628/prsnl.site/brianlovincom_tmvg1d.png", "dateUpdated": "3/26/2022"},
+    {"url": "https://aschmelyun.com", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649297628/prsnl.site/aschemlyuncom_z0hjat.png", "dateUpdated": "3/26/2022"},
+    {"url": "https://vitalik.ca", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649297629/prsnl.site/vitalikca_txemvy.png", "dateUpdated": "3/26/2022"},
+    {"url": "https://swyx.io", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649297629/prsnl.site/swyxio_di56dq.png", "dateUpdated": "04/05/2022"},
+    {"url": "https://joshwcomeau.com", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649297628/prsnl.site/joshwcomeaucom_efuelp.png", "dateUpdated": "04/05/2022"},
+    {"url": "https://jvns.ca", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649297630/prsnl.site/jvnsca_chaap4.png", "dateUpdated": "04/05/2022"},
+    {"url": "https://calebporzio", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649297628/prsnl.site/calebporziocom_cwe3fs.png", "dateUpdated": "04/05/2022"},
+    {"url": "https://lukesmith.xyz", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649297628/prsnl.site/lukesmithxyz_y7e2az.png", "dateUpdated": "04/06/2022"},
+    {"url": "https://leerob.io", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649297628/prsnl.site/leerobio_iv3pn1.png", "dateUpdated": "04/06/2022"},
+    {"url": "https://pketh.org", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649378602/prsnl.site/pkethorg_f4mpni.png", "dateUpdated": "04/07/2022"},
+    {"url": "https://boz.com", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649378737/prsnl.site/bozcom_uljm2w.png", "dateUpdated": "04/07/2022"},
+    {"url": "https://dustinbrett.com", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649379887/prsnl.site/dustinbrettcom_kxvklj.png", "dateUpdated": "04/07/2022"},
+    {"url": "https://whitep4nth3r.com", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649465773/prsnl.site/whitep4nth3rcom_wtgdmn.png", "dateUpdated": "04/08/2022"},
+    {"url": "https://adamconrad.dev", "img": "https://res.cloudinary.com/shoveltoss-com/image/upload/v1649465890/prsnl.site/adamconraddev_chnqzv.png", "dateUpdated": "04/08/2022"}
+]
